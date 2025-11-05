5b54d5a78935ab772f23755fa9735e03e1d6c926
YarGrishaHub <gaming.hub96@mail.ru>
2025-11-05 18:34:10 +0300

    second page

--- app/src/main/java/com/example/myapplicationshop/SecondActivity2.kt
+++ app/src/main/java/com/example/myapplicationshop/SecondActivity2.kt
@@ -5,19 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.content.Intent
-import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val btn_start = findViewById<Button>(R.id.btnStart)
 
-        btn_start.setOnClickListener {
-            val intent = Intent(this, SecondActivity2::class.java)
-            startActivity(intent)
-        }
     }
 }
